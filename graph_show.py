02525e6b3d1bb5e67ba067168d6c2bfad8d85814
Today017 <yesterday998244353@gmail.com>
2022-11-09 00:26:02 +0900

    edit graph_show.py

--- graph_show.py
+++ graph_show.py
@@ -1,6 +1,9 @@
 from matplotlib import pyplot as pyp
-n = int(input())
-a = list(map(int, input().split()))
+
+f = open('out.txt', 'r')
+
+n = int(f.readline())
+a = list(map(int, f.readline().split()))
 
 pyp.plot(a)
 pyp.show()
